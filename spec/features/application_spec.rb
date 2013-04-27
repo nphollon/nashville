0431d1290f7c5a50fdde6bb85f82d139e089d91a
nphollon <nhollon@thoughtworks.com>
2013-04-27 18:22:21 -0500

    fun with CSS

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -7,41 +7,37 @@ describe "Application", :type => :feature, :js => true do
 
     it "has a greeting and links to a 'You have won' screen" do
       page.should have_content("Hello!")
-      page.should have_selector("a", text: "Deal")
+      page.should have_selector("#action", text: "Deal")
       page.should_not have_content("You have won")
-      page.should_not have_selector("a", text: "OK")
+      page.should_not have_content("You have lost")
     end
 
     it "links to a 'You have won' screen if next generated value is a 1" do
       Random.any_instance.stub(:rand).and_return(1)
-      click_link 'Deal'
+      find('#action').click
 
       page.should_not have_content("Hello!")
-      page.should_not have_selector("a", text: "Deal")
       page.should have_content("You have won")
-      page.should have_selector("a", text: "OK")
-      click_link 'OK'
+      page.should have_selector("#action", text: "OK")
+      find('#action').click
       
       page.should have_content("Hello!")
-      page.should have_selector("a", text: "Deal")
+      page.should have_selector("#action", text: "Deal")
       page.should_not have_content("You have won")
-      page.should_not have_selector("a", text: "OK")
     end
 
     it "links to a 'You have lost' screen if next generated value is a 0" do
       Random.any_instance.stub(:rand).and_return(0)
-      click_link 'Deal'
+      find('#action').click
 
       page.should_not have_content("Hello!")
-      page.should_not have_selector("a", text: "Deal")
       page.should have_content("You have lost")
-      page.should have_selector("a", text: "OK")
-      click_link 'OK'
+      page.should have_selector("#action", text: "OK")
+      find('#action').click
       
       page.should have_content("Hello!")
-      page.should have_selector("a", text: "Deal")
+      page.should have_selector("#action", text: "Deal")
       page.should_not have_content("You have lost")
-      page.should_not have_selector("a", text: "OK")
     end
   end
 end
