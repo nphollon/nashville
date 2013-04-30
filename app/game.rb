71d59276fb0417e69f0c555d793952c858f3f00d
nphollon <nhollon@thoughtworks.com>
2013-04-29 19:17:48 -0500

    Game cycles between states

--- app/game.rb
+++ app/game.rb
@@ -2,27 +2,39 @@ module Nashville
   class Game
     USER_VICTORY = 1
     NUMBER_OF_PLAYERS = 2
+    WELCOME_STRING = "Hello!"
     VICTORY_STRING = "You have won"
     DEFEAT_STRING = "You have lost"
 
-    attr_reader :rng, :outcome
-    private :rng, :outcome
+    attr_accessor :state_string, :outcome
+    attr_reader :rng
+    private :rng, :outcome, :outcome=
 
     def initialize(rng)
       @rng = rng
     end
 
     def play
-      @outcome = rng.rand(NUMBER_OF_PLAYERS)
+      self.outcome = rng.rand(NUMBER_OF_PLAYERS)
+    end
+
+    def next_state
+      self.state_string = case @state_string
+      when WELCOME_STRING
+        play
+        result_string
+      else
+        WELCOME_STRING
+      end
+    end
+
+    def result_string
+      user_won? ? VICTORY_STRING : DEFEAT_STRING
     end
 
     def user_won?
       raise NoGamePlayedError unless outcome
       outcome == USER_VICTORY
-    end
-
-    def result_string
-      user_won? ? VICTORY_STRING : DEFEAT_STRING
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,9 @@ require_relative '../app/game'
 describe "Game" do
   let(:rng) { Random.new }
   let(:game) { Nashville::Game.new(rng) }
+  subject { game }
+
+  its(:state_string) { should be_nil }
 
   describe "play" do
     it "should call rand with a choice of 2" do
@@ -37,6 +40,39 @@ describe "Game" do
     it "should return 'You have lost' if user lost" do
       game.stub(:user_won?).and_return(false)
       game.result_string.should == "You have lost"
+    end
+  end
+
+  describe "next_state" do
+    it "should set state_string to 'Hello!' if state_string is nil" do
+      game.next_state
+      game.state_string.should == 'Hello!'
+    end
+
+    it "should set state_string to 'You have won' if state_string is 'Hello!' and next generated value is 1" do
+      rng.stub(:rand).and_return(1)
+      game.state_string = 'Hello!'
+      game.next_state
+      game.state_string.should == 'You have won'
+    end
+
+    it "should set state_string to 'You have lost' if state_string is 'Hello!' and next generated value is 0" do
+      rng.stub(:rand).and_return(0)
+      game.state_string = 'Hello!'
+      game.next_state
+      game.state_string.should == 'You have lost'
+    end
+
+    it "should set state_string to 'Hello!' if state_string is 'You have won'" do
+      game.state_string = 'You have won'
+      game.next_state
+      game.state_string.should == 'Hello!'
+    end
+
+    it "should set state_string to 'Hello!' if state_string is 'You have lost'" do
+      game.state_string = 'You have lost'
+      game.next_state
+      game.state_string.should == 'Hello!'
     end
   end
 end
