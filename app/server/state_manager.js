ab346c5c54fd2d0eaa69604952ea58028327f1e1
nphollon <nhollon@thoughtworks.com>
2014-05-07 19:51:18 -0700

    Error handling for state manager

--- app/server/state_manager.js
+++ app/server/state_manager.js
@@ -1,36 +1,78 @@
 "use strict";
 
-exports.build = function () {
-  var stateManager = {}
-
-  stateManager.initialize = function (callback) {
-    var initialState = {
+exports.createState = function (original) {
+  if (original === undefined) {
+    return {
       needChanceEvent: false,
       wager: 1,
       score: 0,
       status: ""
     }
+  } else {
+    return {
+      needChanceEvent: original.needChanceEvent,
+      wager: original.wager,
+      score: original.score,
+      status: original.status
+    }  
+  }
+}
+
+exports.build = function () {
+  var stateManager = {}
+
+  var adjustScore = function (state, decision, callback) {
+    state.score += state.wager * (decision.userWins ? 1 : -1)
+    state.needChanceEvent = false
 
     process.nextTick(function () {
-      callback(null, initialState)
+      callback(null, state)
+    })
+  }
+
+  var setWager = function (state, decision, callback) {
+    state.wager = decision.wager
+    state.needChanceEvent = true
+    process.nextTick(function () {
+      callback(null, state)
+    })
+  }
+
+  var raiseStateError = function (state, decision, callback) {
+    process.nextTick(function () {
+      var error = new Error("State manager received invalid decision.")
+      error.decision = decision
+      error.state = state
+      callback(error)
+    })
+  }
+
+  stateManager.initialize = function (callback) {
+    process.nextTick(function () {
+      callback(null, exports.createState())
     })
   }
 
   stateManager.advance = function (previousState, decision, callback) {
-    var nextState = {
-      needChanceEvent: !previousState.needChanceEvent,
-      wager: previousState.wager,
-      score: previousState.score,
-      status: ""
+    var nextState = exports.createState(previousState)
+
+    var mutators = {
+      true: adjustScore,
+      false: setWager
     }
-    if (decision.userWins === undefined) {
-      nextState.wager = decision.wager
+
+    var neededProperty = {
+      true: "userWins",
+      false: "wager"
+    }
+
+    var key = nextState.needChanceEvent
+
+    if (decision.hasOwnProperty(neededProperty[key])) {
+      mutators[key].call(null, nextState, decision, callback)
     } else {
-      nextState.score += nextState.wager * (decision.userWins ? 1 : -1)
+      raiseStateError(nextState, decision, callback)
     }
-    process.nextTick(function () {
-      callback(null, nextState)
-    })
   }
 
   return stateManager
--- spec/state_manager_spec.js
+++ spec/state_manager_spec.js
@@ -12,9 +12,7 @@ describe("The state manager", function () {
   it("should initialize the game state", function (done) {
     var callback = function (error, data) {
       expect(error).toBe(null)
-      expect(data.needChanceEvent).toBe(false)
-      expect(data.wager).toBe(1)
-      expect(data.score).toBe(0)
+      expect(data).toEqual(stateManagerFactory.createState())
       done()
     }
 
@@ -22,15 +20,8 @@ describe("The state manager", function () {
   })
 
   it("should request chance event after client makes a wager", function (done) {
-    var decision = {
-      wager: 3
-    }
-
-    var initialState = {
-      needChanceEvent: false,
-      wager: 1,
-      score: 0
-    }
+    var decision = { wager: 3 }
+    var initialState = { needChanceEvent: false, wager: 1, score: 0 }
 
     var callback = function (error, data) {
       expect(error).toBe(null)
@@ -44,15 +35,8 @@ describe("The state manager", function () {
   })
 
   it("should deduct wager from score if chance event is false", function (done) {
-    var chanceEvent = {
-      userWins: false
-    }
-
-    var initialState = {
-      needChanceEvent: true,
-      wager: 3,
-      score: 5
-    }
+    var decision = { userWins: false }
+    var initialState = { needChanceEvent: true, wager: 3, score: 5 }
 
     var callback = function (error, data) {
       expect(error).toBe(null)
@@ -62,19 +46,12 @@ describe("The state manager", function () {
       done()
     }
 
-    stateManager.advance(initialState, chanceEvent, callback)
+    stateManager.advance(initialState, decision, callback)
   })
 
   it("should add wager to score if chance event is true", function (done) {
-    var chanceEvent = {
-      userWins: true
-    }
-
-    var initialState = {
-      needChanceEvent: true,
-      wager: 3,
-      score: 5
-    }
+    var decision = { userWins: true }
+    var initialState = { needChanceEvent: true, wager: 3, score: 5 }
 
     var callback = function (error, data) {
       expect(error).toBe(null)
@@ -84,9 +61,34 @@ describe("The state manager", function () {
       done()
     }
 
-    stateManager.advance(initialState, chanceEvent, callback)
+    stateManager.advance(initialState, decision, callback)
   })
 
-  it("should return error if an unexpected chance event is received")
-  it("should return error if an unexpected client event is received")
+  it("should return error if an unexpected chance event is received", function (done) {
+    var decision = { userWins: true }
+    var initialState = { needChanceEvent: false, wager: 3, score: 5, status: "" }
+
+    var callback = function (error) {
+      expect(error.message).toBe("State manager received invalid decision.")
+      expect(error.decision).toEqual(decision)
+      expect(error.state).toEqual(initialState)
+      done()
+    }
+
+    stateManager.advance(initialState, decision, callback)
+  })
+
+  it("should return error if an unexpected client event is received", function (done) {
+    var decision = { wager: 3 }
+    var initialState = { needChanceEvent: true, wager: 3, score: 5, status: "" }
+
+    var callback = function (error) {
+      expect(error.message).toBe("State manager received invalid decision.")
+      expect(error.decision).toEqual(decision)
+      expect(error.state).toEqual(initialState)
+      done()
+    }
+
+    stateManager.advance(initialState, decision, callback)    
+  })
 })
