367105594c56fc18eacf1892e9c3d88cf6f12f36
nphollon <nhollon@thoughtworks.com>
2014-05-12 22:30:21 -0700

    Moved dependency management into Depdep module

--- app/server/application.js
+++ app/server/application.js
@@ -1,81 +1,54 @@
 "use strict";
 
-var defaults = {
-  webServer: {
-    dependencies: ["router"],
-    factory: function (deps) {
-      return require("http").createServer(deps.router)
+var defaultFactories = {
+  webServer: function (that) {
+    return require("http").createServer(that.router)
+  },
+
+  router: function (that) {
+    return require("./router").build(that.routes)
+  },
+
+  routes: function (that) {
+    var get = that.routeFactory.get
+    var post = that.routeFactory.post
+    return {
+      "/": get("public/index.html", "text/html"),
+      "/index.css": get("public/index.css", "text/css"),
+      "/index.js": get("public/index.js", "application/javascript"),
+      "/request-update": post(that.adapter.requestUpdate),
+      "/submit-decision": post(that.adapter.submitDecision)
     }
   },
 
-  router: {
-    dependencies: ["routes"],
-    factory: function (deps) {
-      return require("./router").build(deps.routes)
-    }
+  routeFactory: function () {
+    return require("./routes")
   },
 
-  routes: {
-    dependencies: ["routeFactory", "adapter"],
-    factory: function (deps) {
-      var get = deps.routeFactory.get
-      var post = deps.routeFactory.post
-      return {
-        "/": get("public/index.html", "text/html"),
-        "/index.css": get("public/index.css", "text/css"),
-        "/index.js": get("public/index.js", "application/javascript"),
-        "/request-update": post(deps.adapter.requestUpdate),
-        "/submit-decision": post(deps.adapter.submitDecision)
-      }
-    }
+  adapter: function (that) {
+    return require("./ajax_adapter").build(that.dispatcher)
   },
 
-  routeFactory: {
-    dependencies: [],
-    factory: function () {
-      return require("./routes")
-    }
+  dispatcher: function () {
+    return require("./dispatcher").build()
   },
 
-  adapter: {
-    dependencies: ["dispatcher"],
-    factory: function (deps) {
-      return require("./ajax_adapter").build(deps.dispatcher)
-    }
+  gameServer: function (that) {
+    return require("./game_server").build(
+      that.dispatcher,
+      that.stateManager,
+      that.chancePlayer
+    )
   },
 
-  dispatcher: {
-    dependencies: [],
-    factory: function () {
-      return require("./dispatcher").build()
-    }
+  stateManager: function () {
+    return require("./state_manager").build()
   },
 
-  gameServer: {
-    dependencies: ["dispatcher", "stateManager", "chancePlayer"],
-    factory: function (deps) {
-      return require("./game_server").build(
-        deps.dispatcher,
-        deps.stateManager,
-        deps.chancePlayer
-      )
-    }
-  },
-
-  stateManager: {
-    dependencies: [],
-    factory: function () {
-      return require("./state_manager").build()
-    }
-  },
-
-  chancePlayer: {
-    dependencies: [],
-    factory: function () {
-      return {
-        getNextEvent: function () {
-          return require("./game_events").chanceEvent(true)
-        }
+  chancePlayer: function () {
+    return {
+      getNextEvent: function () {
+        return require("./game_events").chanceEvent(true)
       }
     }
   }
@@ -84,25 +57,8 @@ var defaults = {
 exports.build = function (substitutions) {
   var application = {}
 
-  var objects = {}
-
-  var buildObject = function (name) {
-    if (objects.hasOwnProperty(name)) {
-      return
-    }
-
-    if (substitutions && substitutions.hasOwnProperty(name)) {
-      objects[name] = substitutions[name]
-      return
-    }
-
-    defaults[name].dependencies.forEach(buildObject)
-
-    objects[name] = defaults[name].factory(objects)
-  }
-
-  Object.keys(defaults).forEach(buildObject)
-
+  var objects = require("depdep").buildApplicationContext(defaultFactories, substitutions)
+  
   application.start = function (port) {
     objects.gameServer.start()
     objects.webServer.listen(port)
--- spec/server/application_spec.js
+++ spec/server/application_spec.js
@@ -1,74 +1,42 @@
 describe("the application", function () {
   "use strict";
 
-  var http = require("http")
+  var dependencyManager = require("depdep")
+  
   var helpers = require("../spec_helper")
   var applicationFactory = helpers.requireSource("server/application")
 
-  var gameServer, webServer, port
+  var gameServer, webServer, port, expectedSubs
 
   beforeEach(function () {
+    port = 1
     gameServer = jasmine.createSpyObj("game server", ["start"])
     webServer = jasmine.createSpyObj("web server", ["listen", "close"])
+    expectedSubs = helpers.dummy()
 
-    spyOn(http, "createServer").and.returnValue(webServer)
+    var context = { gameServer: gameServer, webServer: webServer }
 
-    port = 1
+    spyOn(dependencyManager, "buildApplicationContext").and.callFake(
+      function (defaultContext, subs) {
+        return (subs === expectedSubs) ? context : undefined
+      }
+    )
   })
 
   describe("starting", function () {
-    it("should start declared game server and web server", function () {
-      http.createServer.and.returnValue(null)
-
-      var application = applicationFactory.build({
-        gameServer: gameServer,
-        webServer: webServer
-      })
+    it("should start servers received from dependency manager", function () {
+      var application = applicationFactory.build(expectedSubs)
 
       application.start(port)
 
       expect(gameServer.start).toHaveBeenCalled()
       expect(webServer.listen).toHaveBeenCalledWith(port)
-    })
-
-    it("should start default servers if none are specified", function () {
-      var gameServerFactory = helpers.requireSource("server/game_server")
-      spyOn(gameServerFactory, "build").and.returnValue(gameServer)
-
-      var application = applicationFactory.build()
-
-      application.start(port)
-
-      expect(gameServer.start).toHaveBeenCalled()
-      expect(webServer.listen).toHaveBeenCalledWith(port)
-    })
-
-    it("should inject declared dependencies into defaults", function () {
-      var router = helpers.dummy()
-
-      var application = applicationFactory.build({
-        router: router
-      })
-
-      application.start(port)
-
-      expect(http.createServer).toHaveBeenCalledWith(router)
-    })
-
-    it("should not initialize default dependencies more than once", function () {
-      var dispatcherFactory = helpers.requireSource("server/dispatcher")
-      spyOn(dispatcherFactory, "build").and.callThrough()
-      
-      var application = applicationFactory.build()
-      application.start(port)
-
-      expect(dispatcherFactory.build.calls.count()).toBe(1)
     })
   })
 
   describe("stopping", function () {
     it("should close the web server", function () {
-      var application = applicationFactory.build()
+      var application = applicationFactory.build(expectedSubs)
 
       application.stop()
 
