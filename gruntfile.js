6f3d925c11626f90dd937ed42853c060c8f173f1
Nick Hollon <nhollon@thoughtworks.com>
2014-09-15 20:08:04 -0700

    Move index.scss to public-gen

--- gruntfile.js
+++ gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          "public/index.css": "public/index.scss"
+          "public/index.css": "public-gen/index.scss"
         }
       }
     },
